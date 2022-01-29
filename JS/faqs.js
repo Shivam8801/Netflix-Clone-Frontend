e95520a3f4bb53ac14b4eea8a8ba768401335d76
Shivam Nikam <shivammh15@gmail.com>
2022-01-29 16:25:57 +0530

    Add files via upload

---
+++ JS/faqs.js
@@ -0,0 +1,115 @@
+function myFunction1() {
+
+    var x = document.getElementById("ans1");
+    var y = document.getElementById("ans4");
+    var z = document.getElementById("ans2");
+    var a = document.getElementById("ans3");
+    var b = document.getElementById("ans5");
+    var c = document.getElementById("ans6");
+
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("ans2");
+    var y = document.getElementById("ans1");
+    var z = document.getElementById("ans4");
+    var a = document.getElementById("ans3");
+    var b = document.getElementById("ans5");
+    var c = document.getElementById("ans6");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("ans3");
+    var y = document.getElementById("ans1");
+    var z = document.getElementById("ans2");
+    var a = document.getElementById("ans4");
+    var b = document.getElementById("ans5");
+    var c = document.getElementById("ans6");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("ans4");
+    var y = document.getElementById("ans1");
+    var z = document.getElementById("ans2");
+    var a = document.getElementById("ans3");
+    var b = document.getElementById("ans5");
+    var c = document.getElementById("ans6");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("ans5");
+    var y = document.getElementById("ans1");
+    var z = document.getElementById("ans2");
+    var a = document.getElementById("ans3");
+    var b = document.getElementById("ans4");
+    var c = document.getElementById("ans6");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction6() {
+    var x = document.getElementById("ans6");
+    var y = document.getElementById("ans1");
+    var z = document.getElementById("ans2");
+    var a = document.getElementById("ans3");
+    var b = document.getElementById("ans5");
+    var c = document.getElementById("ans4");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
